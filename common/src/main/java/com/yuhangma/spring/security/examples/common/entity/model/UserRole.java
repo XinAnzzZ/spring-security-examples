2b1bfef22f9fa480625a88c166fa541428826b49
moore@xingren.com <xinan950806>
2019-08-04 21:28:57 +0800

    添加 service 模块

--- common/src/main/java/com/yuhangma/spring/security/examples/common/entity/model/UserRole.java
+++ common/src/main/java/com/yuhangma/spring/security/examples/common/entity/model/UserRole.java
@@ -17,7 +17,7 @@ public class UserRole {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private Long id;
 
     private Integer userId;
 
---
+++ common/src/main/java/com/yuhangma/spring/security/examples/common/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.yuhangma.spring.security.examples.common.repository;
+
+import com.yuhangma.spring.security.examples.common.entity.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Moore
+ * @since 2019-08-04
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
